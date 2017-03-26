55bd0ef8272f5d3a7fcdac1244aadf46b9c686fa
wangxl <fpsoundless@gmail.com>
2017-03-26 02:09:39 -0400

    Toggle `Clock` 1 example

--- src/components/03-Clock-state.js
+++ src/components/03-Clock-state.js
@@ -16,13 +16,13 @@ class Clock extends Component {
                 date: new Date()
             }));
         },1000);
-        console.log('*************');
+        console.log(`Mount   ${this.props.name}`);
     }
     componentWillUnmount () {
         clearInterval(this.timer);
         //While this.props is set up by React itself and this.state has a special meaning, you are free to add additional fields to the class manually if you need to store something that is not used for the visual output.
         //If you don't use something in render(), it shouldn't be in the state.  ???
-        console.log('-------------');
+        console.log(`UnMount ${this.props.name}`);
     }
      render () {
         return (
@@ -50,9 +50,8 @@ class ClockOut extends Component {
         const toggle = this.state.toggle;
         return (
             <div>
-                {toggle && <Clock name="react"/> }
                 <input name='isGoing' type="checkbox" checked={toggle} onChange={this.handleChange}/>
-                {toggle || <Clock name="vue"/> }
+                {toggle ? <Clock name="vue"/> : <div><Clock name="react"/></div> }
             </div>
         )
     }
